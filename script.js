5db9e40829a34e860984d23be90bd964dfe4f4f5
João Pedro T <joaopedrot1912@gmail.com>
2023-02-03 20:30:21 -0300

    update 03/02

---
+++ script.js
@@ -0,0 +1,11 @@
+(function(){
+    let conteiner = document.getElementById("conteiner")
+    conteiner.addEventListener("click",function(event){
+        let botao = event.target.id    
+        if(botao == "botaoLigar") {
+            
+        } else if(botao == "botaoDesligar") {
+            
+        }
+    })
+})()
