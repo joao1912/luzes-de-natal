a69ab33872214e633303016e9249bd45135cb6c0
joão <joaopedrot1912@gmail.com>
2023-02-20 19:27:23 -0300

    piscas funcionando e botao desliga feito

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 (function(){
     let conteiner = document.getElementById("conteiner")
+    const piscas = document.querySelectorAll(".circle")
+
+
     conteiner.addEventListener("click",function(event){
         let botao = event.target.id    
         if(botao == "botaoLigar") {
             
         } else if(botao == "botaoDesligar") {
-            
+            [...piscas].forEach( pisca => {
+                pisca.style.animation = "none"
+                pisca.style.backgroundColor = "gray"
+            })
         }
     })
 })()
